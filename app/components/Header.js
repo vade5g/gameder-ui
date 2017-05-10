e1f8e95cd353e295399e6419b7455e07b899adb4
Valtteri Kukkola <valtteri.kukkola@gmail.com>
2017-05-10 17:04:17 +0300

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component{
     return(
       <div className="container-fluid">
         <div className="jumbotron center">
-          <img className='logo-img img-responsive center-block ' src='https://lh3.googleusercontent.com/J8c8eoKd0rX_pszq2gZr29gU6uFC9BN1f8dY_LnxyTvDyCffaqj26lHbhNNwjn2IAUjqFCudsAkLFug=w2648-h1926'></img>
+          <img className='logo-img img-responsive center-block ' src='https://ibin.co/3LzWDPPXBcpa.png'></img>
           <p>Tinder for Gamers</p>
         </div>
       </div>
