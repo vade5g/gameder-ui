7c696e6e5c5628cf155fb524f8f1bf3b7e4d90b2
Aelitta Ezugbaya <aelittae@gmail.com>
2017-05-10 22:47:58 +0300

    added function delete for matches list

--- app/components/ListGroup/MatchesListGroup.js
+++ app/components/ListGroup/MatchesListGroup.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 import MatchesProfile from './MatchesProfile';
 
 export default class MatchesListGroup extends Component{
+  constructor(props){
+    super(props);
+
+    this.deleteProfile = this.deleteProfile.bind(this)
+  }
+
+  deleteProfile(ev, index){
+    if(index != undefined)
+      this.props.updateProfile(index)
+  }
 
   renderProfiles(){
     let profiles = this.props.profiles;
@@ -10,7 +20,7 @@ export default class MatchesListGroup extends Component{
 
     profiles.forEach((user,index)=>{
       list.push(
-          <MatchesProfile key = {index} user = {user}/>
+          <MatchesProfile key = {index} index={index} user = {user} onClick={this.deleteProfile}/>
       )
     });
     return list;
--- app/components/ListGroup/MatchesProfile.js
+++ app/components/ListGroup/MatchesProfile.js
@@ -8,12 +8,16 @@ export default class MatchesProfile extends Component{
     const img = this.props.user.img;
     const email = this.props.user.email;
     const desc = this.props.user.desc;
+    const index = this.props.index;
     return(
       <li className="list-group-item row">
         <div className='col-md-2 col-xs-5'>
           <img src={img} className="img-circle matchImg"/>
         </div>
         <div className='col-md-10 col-xs-7'>
+        <a href='#'  onClick={ev => this.props.onClick(ev, index)}  className="pull-right">
+          <i className='fa fa-times'/>
+        </a>
           <h3>{` ${name}`}</h3>
           <h4>{email}</h4>
           <h5>{desc}</h5>
@@ -25,4 +29,5 @@ export default class MatchesProfile extends Component{
 
 MatchesProfile.propTypes={
   user: PropTypes.shape(UserShape),
+  index:PropTypes.number,
 }
