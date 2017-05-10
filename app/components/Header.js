7e454708c8988604887f565200736df6a63d236e
noiresonn <noiresonn@gmail.com>
2017-05-10 16:56:49 +0300

    Update the logo link

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component{
     return(
       <div className="container-fluid">
         <div className="jumbotron center">
-          <img className='logo-img img-responsive center-block ' src='https://lh6.googleusercontent.com/fTR_mxOSHw-L9TWuOXEB6sVdUw45wOIs8C_Pgb0wRUUfxi2xbvsWfuTYNNk1qtskc1MFrFS9L4QLGQ4=w2648-h1926'></img>
+          <img className='logo-img img-responsive center-block ' src='https://lh3.googleusercontent.com/J8c8eoKd0rX_pszq2gZr29gU6uFC9BN1f8dY_LnxyTvDyCffaqj26lHbhNNwjn2IAUjqFCudsAkLFug=w2648-h1926'></img>
           <p>Tinder for Gamers</p>
         </div>
       </div>
