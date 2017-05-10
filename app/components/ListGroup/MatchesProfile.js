1c078b4531a4171b6278cfca059d60ebf6ce014a
Aelitta Ezugbaya <aelittae@gmail.com>
2017-05-10 18:09:20 +0300

    added description for profiles in descriptions

--- app/components/ListGroup/MatchesProfile.js
+++ app/components/ListGroup/MatchesProfile.js
@@ -7,14 +7,16 @@ export default class MatchesProfile extends Component{
     const name = this.props.user.name;
     const img = this.props.user.img;
     const email = this.props.user.email;
+    const desc = this.props.user.desc;
     return(
       <li className="list-group-item row">
-        <div className='col-md-3'>
+        <div className='col-md-2 col-xs-5'>
           <img src={img} className="img-circle matchImg"/>
         </div>
-        <div className='col-md-9'>
-          <h4>{`Username: ${name}`}</h4>
-          <p>{email}</p>
+        <div className='col-md-10 col-xs-7'>
+          <h3>{` ${name}`}</h3>
+          <h4>{email}</h4>
+          <h5>{desc}</h5>
         </div>
       </li>
     );
